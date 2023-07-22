f41bd26459166b1ae64a3a5b5151e7e686781b5d
lathiesh24 <lathumahi24@gmail.com>
2023-07-23 02:35:48 +0530

    recent

--- app.js
+++ app.js
@@ -1,18 +1,1 @@
-gsap.registerPlugin(ScrollTrigger);
-
-const tl = gsap.timeline();
-
-tl.to(".box",{
-     x: 800,
-     duration: 2,
-})
-
-tl.to(".box",{
-    y:200,
-    duration: 3,
-})
-
-tl.to(".box",{
-    x:0,
-    duration: 3,
-})
+gsap.registerPlugin(ScrollTrigger)
