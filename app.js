d95b748d1dd966dbfefb6638d216dc86e18889e1
lathiesh24 <lathumahi24@gmail.com>
2023-07-22 17:10:34 +0530

    recent

--- app.js
+++ app.js
@@ -1,33 +1,7 @@
-gsap.to(".paper-plane", {
-    duration:2,
-    x:300,
-    backgroundColor:"white",
-    borderRadius: "32px",
-    border:"1 px solid transparent", 
-    ease: "steps (4)",
-}); 
+gsap.registerPlugin(ScrollTrigger);
 
-gsap.to(".paper-plane2", {
-    duration:2,
-    x:300,
-    backgroundColor:"white",
-    borderRadius: "32px",
-    border:"1 px solid transparent", 
-    ease: "steps (4)",
-}); 
-
-gsap.set(".paper-plane " ,{ transformOrigin:"20% 20%"}); 
-
-gsap.set(".paper-plane2" ,{ transformOrigin:"50% 50%"}); 
-
-gsap.to(".paper-plane",{
-    duration: 2,
-    rotation: 360
+gsap.to(".square",{
+     x: 800,
+     duration: 2,
+     scrollTrigger: ".square2"
 })
-
-gsap.to(".paper-plane2",{
-    duration: 2,
-    rotation: 360
-})
-
-
